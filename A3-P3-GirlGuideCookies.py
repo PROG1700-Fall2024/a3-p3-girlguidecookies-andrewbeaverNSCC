5676eb3bef54d2a5908530a6753f4e00a2f4306d
Your Name <w0402993@nscc.ca>
2024-11-19 22:23:03 -0400

    Ready for marking

--- A3-P3-GirlGuideCookies.py
+++ A3-P3-GirlGuideCookies.py
@@ -29,39 +29,46 @@ def main():
     avgSold = 0
 
     #Get the numbers the guides
-    numGuides = int(input("\nEnter the number of guides selling cookies: "))
+    numGuides = int((input("\nEnter the number of guides selling cookies: ")))
 
-    #Use the number of guides to create a loop
-    #Get names and cookies sold and add to lists
-    for i in range(numGuides):
-        guideName = input("\nEnter the name of guide #{0}: ".format(i + 1))
-        cookiesSold = int(input("Enter the number of boxes sold by {0}: ".format(guideName)))
-        guideList.append([guideName, cookiesSold])
+    #Handle if the user inputs a 0 or negative for the number of guides
+    if numGuides > 0:
+        #Use the number of guides to create a loop
+        #Get names and cookies sold and add to lists
+        for i in range(numGuides):
+            guideName = input("\nEnter the name of guide #{0}: ".format(i + 1))
+            cookiesSold = int(input("Enter the number of boxes sold by {0}: ".format(guideName)))
+        #Error handle to let user know they can't have a negative
+            if cookiesSold < 0:
+                print("WARNING. You can't make negative sales")
+            guideList.append([guideName, cookiesSold])
 
-    #Calculate average cookies sold
-    totalSales = sum(salesTotal[1] for salesTotal in guideList)
-    avgSold = totalSales / numGuides
+        #Calculate average cookies sold
+        totalSales = sum(salesTotal[1] for salesTotal in guideList)
+        avgSold = totalSales / numGuides
 
 
-    highestSales = findHighestSeller(guideList)
+        highestSales = findHighestSeller(guideList)
 
-    #Display average cookies sold
-    print("\nThe average number of boxes sold by each guide was {0:.1f}".format(avgSold))
+        #Display average cookies sold
+        print("\nThe average number of boxes sold by each guide was {0:.1f}".format(avgSold))
 
-    print("\nGuide\t\tPrizes Won:\n")
-    
-    #Establish prize winners, best seller gets trip, above avg gets badge, others with sales get left overs
-    for i in range (numGuides):
-        guideName, cookiesSold = guideList[i]
-        if cookiesSold == highestSales:
-            prize = winnerPrize
-        elif cookiesSold > avgSold:
-            prize = aboveAvgPrize
-        elif cookiesSold > leftOversLowerBoundary and cookiesSold <= avgSold:
-            prize = leftOvers
-        else:
-            prize = ""
-    
-        print("{0}\t\t- {1}".format(guideName, prize))
+        print("\nGuide\t\tPrizes Won:\n")
+        
+        #Establish prize winners, best seller gets trip, above avg gets badge, others with sales get left overs
+        for i in range (numGuides):
+            guideName, cookiesSold = guideList[i]
+            if cookiesSold == highestSales:
+                prize = winnerPrize
+            elif cookiesSold > avgSold:
+                prize = aboveAvgPrize
+            elif cookiesSold > leftOversLowerBoundary and cookiesSold <= avgSold:
+                prize = leftOvers
+            else:
+                prize = ""
+        
+            print("{0}\t\t- {1}".format(guideName, prize))
+    else:
+        print("Invalid girl guide number added")
 
 main()
